2a57b3c00334ac9b194b7365a765ae82b63659ed
Alex Nagy <therebeloper@gmail.com>
2021-03-19 18:28:37 +0200

    custom button

--- Sources/MySPLibrary/MySPLibrary.swift
+++ Sources/MySPLibrary/MySPLibrary.swift
@@ -1,3 +1,21 @@
-struct MySPLibrary {
-    var text = "Hello, World!"
+//
+//  MySPLibrary.swift
+//  
+//
+//  Created by Alex Nagy on 19.03.2021.
+//
+
+import SwiftUI
+
+public struct CustomButtonStyle: ButtonStyle {
+    public func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.title)
+            .foregroundColor(.white)
+            .background(
+                RoundedRectangle(cornerRadius: 15)
+                    .fill(Color.green)
+            )
+            .opacity(configuration.isPressed ? 0.5 : 1.0)
+    }
 }
--- Tests/MySPLibraryTests/MySPLibraryTests.swift
+++ Tests/MySPLibraryTests/MySPLibraryTests.swift
@@ -6,7 +6,7 @@ final class MySPLibraryTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(MySPLibrary().text, "Hello, World!")
+        
     }
 
     static var allTests = [
